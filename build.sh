0d61349b10796d0c4191eebd235554005d853a34
Billy Foss <billy@billyfoss.com>
2020-08-08 15:50:13 -0400

    Track Terragrunt version
    With the current tag reflecting the terraform version, there have been
    no updates to inclue newer versions of terragrunt.
    
    This change clarifies where terragrunt vs terraform version is used.
    It also uses a combined tag so that new builds will be generated when a
    new version of Terraform or Terragrunt are released.

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@
 set -ex
 
 image="alpine/terragrunt"
-repo="hashicorp/terraform"
+terraform_repo="hashicorp/terraform"
+terragrunt_repo="gruntwork-io/terragrunt"
 
 if [[ ${CI} == 'true' ]]; then
   CURL="curl -sL -H \"Authorization: token ${API_TOKEN}\""
@@ -17,30 +18,35 @@ else
   CURL="curl -sL"
 fi
 
-latest=$(${CURL} https://api.github.com/repos/${repo}/releases/latest |jq -r .tag_name|sed 's/v//')
+latest_terraform=$(${CURL} https://api.github.com/repos/${terraform_repo}/releases/latest |jq -r .tag_name|sed 's/v//')
 
-terragrunt=$(${CURL} https://api.github.com/repos/gruntwork-io/terragrunt/releases/latest |jq -r .tag_name)
+latest_terragrunt=$(${CURL} https://api.github.com/repos/${terragrunt_repo}/releases/latest |jq -r .tag_name)
+
+# Define tag for Alpine Terragrunt releases
+latest_alpine_terragrunt=${latest_terragrunt}-tf${latest_terraform}
 
 sum=0
-echo "Lastest release is: ${latest}"
+echo "Latest Terraform release is: ${latest_terraform}"
+echo "Latest Terragrunt release is: ${latest_terragrunt}"
+echo "Latest Alpine Terragrunt tag is: ${latest_alpine_terragrunt}"
 
 tags=`curl -s https://hub.docker.com/v2/repositories/${image}/tags/ |jq -r .results[].name`
 
 for tag in ${tags}
 do
-  if [ ${tag} == ${latest} ];then
+  if [ ${tag} == ${latest_alpine_terragrunt} ];then
     sum=$((sum+1))
   fi
 done
 
 if [[ ( $sum -ne 1 ) || ( ${REBUILD} == "true" ) ]];then
-  sed "s/VERSION/${latest}/" Dockerfile.template > Dockerfile
-  docker build --build-arg TERRAGRUNT=${terragrunt} --no-cache -t ${image}:${latest} .
-  docker tag ${image}:${latest} ${image}:latest
+  sed "s/TERRAFORM_VERSION/${latest_terraform}/" Dockerfile.template > Dockerfile
+  docker build --build-arg TERRAGRUNT_VERSION=${latest_terragrunt} --build-arg TERRAFORM_VERSION=${latest_terraform} --no-cache -t ${image}:${latest_alpine_terragrunt} .
+  docker tag ${image}:${latest_alpine_terragrunt} ${image}:latest
 
   if [[ "$TRAVIS_BRANCH" == "master" ]]; then
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    docker push ${image}:${latest}
+    docker push ${image}:${latest_alpine_terragrunt}
     docker push ${image}:latest
   fi
 
