c87e5be4f4a56a8423b9d07b40e2ec67acefcb94
Bill Wang <bill.wang@bill.wang>
2024-05-30 13:08:36 +1000

    add tests

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker run -ti --rm -v $(pwd)/test/test-terragrunt:/apps -w /apps alpine/terragrunt bash -c "terragrunt --version > result && terragrunt init && terragrunt apply -auto-approve && echo $? >> result"
-
+echo $?
 echo "==== test result ===="
 cat test/test-terragrunt/result
